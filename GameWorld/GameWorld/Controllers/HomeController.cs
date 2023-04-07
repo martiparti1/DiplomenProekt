23bfd3713be5935fed88efacbb83f6b9b9caa5fc
martin <m.petrov2018@pmg-pernik.com>
2023-04-07 10:20:11 +0300

    jamba jumba rando products

--- GameWorld/GameWorld/Controllers/HomeController.cs
+++ GameWorld/GameWorld/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using GameWorld.Models;
+﻿using GameWorld.Data;
+using GameWorld.Models;
+using GameWorld.Models.Product;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -11,16 +13,34 @@ namespace GameWorld.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var randomProducts = _context.Products
+        .OrderBy(x => Guid.NewGuid())
+        .Take(4)
+        .ToList();
+
+            var productIndexVMs = randomProducts
+                .Select(x => new ProductIndexVM
+                {
+                    Id = x.Id,
+                    ProductName = x.ProductName,
+                    Description = x.Description,
+                    Image = x.Image,
+                    Price = x.Price
+                })
+                .ToList();
+
+            return View(productIndexVMs);
         }
 
         public IActionResult Privacy()
